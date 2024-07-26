b3a4babb586d6a74648df585d83433a2e059e88c
Mostafa <mostafa.saneisn@gmail.com>
2024-07-27 01:02:09 +0330

    Add Order Item model to init file

--- apps/products/models/__init__.py
+++ apps/products/models/__init__.py
@@ -1,11 +1,12 @@
 from .category import Category
 from .product import Product
 from .review import Review
-from .order import Order
+from .order import Order, OrderItem
 
 __all__ = [
     'Category',
     'Product',
     'Review',
     'Order',
+    'OrderItem'
 ]
